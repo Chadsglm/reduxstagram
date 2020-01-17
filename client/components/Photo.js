8b9c9c5f32f3a3f42f10c6530cdf5b7e96245c3c
Chad Saglam <saglam.chd@gmail.com>
2020-01-17 14:29:44 +0100

    Updating State with Reducers

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -26,6 +26,7 @@ const Photo = React.createClass({
             <button onClick={this.props.increment.bind(null, index)} 
                     className="likes">&hearts; {post.likes}
             </button>
+            
             <Link className="button" to={`/view/${post.code}`}>
               <span className="comment-count">
                 <span className="speech-bubble"></span>
--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -7,7 +7,7 @@ const PhotoGrid = React.createClass({
       <div className="photo-grid">
         {this.props.posts.map(
           (post, index) => 
-            <Photo {...this.props} key={index} i={index} post={post} />
+            <Photo {...this.props} key={index} index={index} post={post} />
         )}
       </div>
     )
