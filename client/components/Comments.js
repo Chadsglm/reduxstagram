11765b2c49cab63ab6f783aae969b7df4335862e
Chad Saglam <saglam.chd@gmail.com>
2020-01-17 14:39:46 +0100

    Displaying the Single Photo Component

---
+++ client/components/Comments.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Comments = React.createClass({
+  render() {
+    return (
+      <div className="comments">
+        I am the comments!!!!!!!
+      </div>
+    )
+  }
+});
+
+export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
-
-const Single =  React.createClass ({
+const Single = React.createClass({
   render() {
+    const { postId } = this.props.params;
+
+    const index = this.props.posts.findIndex(
+      (post) => post.code === postId
+    );
+    // console.log(index);
+    const post = this.props.posts[index];
+    // console.log(post);
+
+    const postComments = this.props.comments[postId] || [];
+    // console.log(postComments);
+
     return (
       <div className="single-photo">
-        
+        <Photo index={index} post={post} {...this.props} />
+        <Comments />
       </div>
-    );
+    )
   }
 });
 
